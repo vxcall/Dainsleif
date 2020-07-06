4b5559152feab566f88d8ef7b940cf0f42425b90
S3pt3mb3r <33578715+s3pt3mb3r@users.noreply.github.com>
2020-07-06 17:05:09 +0800

    I totally upgrade the AimBot function
    I make AimBot() aim at target gradually.
    I added EucledeanAlgorithm() as well but I don't know if I could handle it properly. It's not been used now but probably It will be used in AimBot function by next commit.

--- LocalPlayer.cpp
+++ LocalPlayer.cpp
@@ -3,9 +3,9 @@
 #include "offsets.h"
 #include <iostream>
 #include <Windows.h>
-#include "vector3.h"
 #include <math.h>
-#include <cmath>
+#include <vector>
+#include <cstdlib>
 
 
 LocalPlayer* GetLocalPlayer(uintptr_t moduleBase)
@@ -19,6 +19,40 @@ float LocalPlayer::GetDistance(Vector3 targetPos, Vector3& deltaVector)
     deltaVector =  targetPos - *this->GetHeadPosition(); //minus operation is defined in vector3.h
 	float distance = sqrt(deltaVector.x * deltaVector.x + deltaVector.y * deltaVector.y + deltaVector.z * deltaVector.z); //This is the trigonometry calculation to get hypnotenuse.
 	return distance;
+}
+
+int EucledeanAlgorithm(int var1, int var2) {
+	var1 = abs(var1);
+	var2 = abs(var2);
+	if (var1 == var2) {
+		return var1;
+	}
+
+	if (var1 < var2)
+	{
+		std::swap(var1, var2);
+	}
+
+	std::vector<int> nr;
+	int reminder = var1 % var2;
+	if (!reminder)
+	{
+		return var2;
+	}
+	nr.push_back(reminder);
+	int nullcheck = var2 % reminder;
+	if (!nullcheck)
+	{
+		return reminder;
+	}
+	nr.push_back(nullcheck);
+	int i = 1;
+	while (1) {
+		nr.push_back(nr[i - 1] % nr[i]);
+		++i;
+		if (nr[i] == 0) break;
+	}
+	return nr[i - 1];
 }
 
 float LocalPlayer::GetDistance(Vector3 targetPos)
@@ -56,6 +90,10 @@ int* LocalPlayer::GetTeam()
 	return reinterpret_cast<int*>((*(uintptr_t*)this + m_iTeamNum));
 }
 
+int sign(float A) {
+	return (A > 0) - (A < 0);
+}
+
 double PI = 3.14159265358;
 void LocalPlayer::AimBot(Vector3 TargetsHeadPosition)
 {
@@ -63,28 +101,60 @@ void LocalPlayer::AimBot(Vector3 TargetsHeadPosition)
 	static Vector3* viewAngles = reinterpret_cast<Vector3*>((*reinterpret_cast<uintptr_t*>((engineModule + dwClientState)) + dwClientState_ViewAngles));
 	Vector3 delta;
 	float hypotenuse = GetDistance(TargetsHeadPosition, delta);
-	float pitch = -asin(delta.z / hypotenuse) * (180 / PI);
-	float yaw = atan2(delta.y, delta.x) * (180 / PI);
+	float pitch = -asin(delta.z / hypotenuse) * (180 / static_cast<float>(PI));
+	float yaw = atan2(delta.y, delta.x) * (180 / static_cast<float>(PI));
 	if (pitch >= -89 && pitch <= 89 && yaw >= -180 && yaw <= 180)
 	{
-		if (viewAngles->x < pitch)
-		{
-			viewAngles->x += 0.1f;
-		}
-		else if (viewAngles->x > pitch)
-		{
-			viewAngles->x -= 0.1f;
-		}
+		int pitchDistance = static_cast<int>(pitch - viewAngles->x);
+		int yawDistance = static_cast<int>(yaw - viewAngles->y);
+		pitchDistance = abs(pitchDistance);
+		yawDistance = abs(yawDistance);
 
-		if (viewAngles->y < yaw)
-		{
-			viewAngles->y += 0.1f;
+		if (pitchDistance <= 0.4f) { //If distance between vertical axis of crosshair and enemy's head is below 0.4 which is pretty close, just aim at exact head.
+			viewAngles->x = pitch;
 		}
-		else if (viewAngles->y > yaw)
+		else 
 		{
-			viewAngles->y -= 0.1f;
+			if (viewAngles->x < pitch)  //my view is smaller than pitch.
+			{
+				viewAngles->x += 0.5f;
+			}
+			else if (viewAngles->x > pitch)
+			{
+				viewAngles->x -= 0.5f;
+			}
 		}
-		//viewAngles->x = pitch;
-		//viewAngles->y = yaw;
+		
+
+		if (yawDistance <= 0.4f) {
+			viewAngles->y = yaw;
+		}
+		else
+		{
+			if (viewAngles->y < yaw)
+			{
+				if (sign(viewAngles->y) == -1 && sign(yaw) == 1 && viewAngles->y <= -90) //When yaw is like 170 and viewAngle steps over -180
+				{
+					viewAngles->y -= 0.5f;
+				}
+				else
+				{
+					viewAngles->y += 0.5f;
+				}
+
+			}
+			else if (viewAngles->y > yaw)
+			{
+				if (sign(viewAngles->y) == 1 && sign(yaw) == -1 && viewAngles->y >= 90)  //When yaw is like -170 and viewAngle steps over 180
+				{
+					viewAngles->y += 0.5f;
+				}
+				else
+				{
+					viewAngles->y -= 0.5f;
+				}
+
+			}
+		}
 	}
 }
