081610da4c3837dded26bf45e46958a1708f7e98
s3pt3mb3r <s3pt3mb3r@protonmail.com>
2020-07-16 16:22:15 +0800

    I added messy detour function.WIP
    I initialized a drawing menu project.

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,6 +1,51 @@
 ﻿#include "pch.h"
 #include "LocalPlayer.h"
+#include <d3d9.h>
+#include <d3dx9.h>
+#include "detours.h"
+//#include "MinHook.h"
 
+using endScene = HRESULT (__stdcall*)(IDirect3DDevice9* pDevice);
+endScene originalEndScene = nullptr; //An original endscene which is null now.
+
+LPD3DXFONT font;
+HRESULT __stdcall hookedEndScene(IDirect3DDevice9* pDevice) { //A function containing a bunch of rendering process, that is gonna be hooked.
+    //now here we can create our own graphics
+    int padding = 2;
+    int rectx1 = 100, rectx2 = 300, recty1 = 50, recty2 = 100;
+    D3DRECT rectangle = { rectx1, recty1, rectx2, recty2 };
+    pDevice->Clear(1, &rectangle, D3DCLEAR_TARGET, D3DCOLOR_ARGB(255, 0, 0, 0), 0.0f, 0); // this draws a rectangle
+    if (!font)
+        D3DXCreateFont(pDevice, 16, 0, FW_BOLD, 1, 0, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_DONTCARE, "Arial", &font);
+    RECT textRectangle;
+    SetRect(&textRectangle, rectx1 + padding, recty1 + padding, rectx2 - padding, recty2 - padding);
+    font->DrawText(NULL, "Press END key to exit", -1, &textRectangle, DT_NOCLIP | DT_LEFT, D3DCOLOR_ARGB(255, 153, 255, 153)); //draw text;
+    return originalEndScene(pDevice);
+}
+
+void hookEndScene() {
+    IDirect3D9* pD3D = Direct3DCreate9(D3D_SDK_VERSION);
+    if (!pD3D) return;
+
+    D3DPRESENT_PARAMETERS d3dparams = {0};  //set D3DPRESENT_PARAMETERS to pass itself as an argument of createDevice().
+    d3dparams.SwapEffect = D3DSWAPEFFECT_DISCARD;
+    d3dparams.hDeviceWindow = GetForegroundWindow();
+    d3dparams.Windowed = true;  //need to be interchangeable
+
+    IDirect3DDevice9* pDevice = nullptr; //A variable to be a device in the next line.
+
+    HRESULT result = pD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, GetForegroundWindow(), D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dparams, &pDevice);
+    if (FAILED(result) || !pDevice){
+        std::cout << "Device is null" << std::endl;
+        pD3D->Release();
+        return;
+    }
+
+    void** vTable = *reinterpret_cast<void***>(pDevice);
+    originalEndScene = (endScene)DetourFunction((PBYTE)vTable[42],(PBYTE)hookedEndScene);
+    pDevice->Release();
+    pD3D->Release();
+}
 
 uintptr_t moduleBase = reinterpret_cast<uintptr_t>(GetModuleHandle("client.dll"));
 
@@ -36,6 +81,7 @@ DWORD __CLRCALL_PURE_OR_STDCALL fMain(HMODULE hMod)
     AllocConsole();
     FILE* f;
     freopen_s(&f, "CONOUT$", "w", stdout);
+    hookEndScene();
     bool bAimbot = false, bGlowHack = false, bNoRecoil = false, bTriggerBot = false;
 
     std::vector<Entity*> entityList;
