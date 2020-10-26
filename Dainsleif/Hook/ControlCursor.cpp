2620029d81521ff6f69e5416c46e1d22d2d8b364
s3pt3mb3r <s3pt3mb3r@protonmail.com>
2020-10-26 17:10:36 +0800

    I fixed a bug causes crash

--- Dainsleif/Hook/ControlCursor.cpp
+++ Dainsleif/Hook/ControlCursor.cpp
@@ -1,8 +1,6 @@
 #include "ControlCursor.h"
 
-#include "Interface.h"
-#include "../PatternScanner.h"
-#include "../dllmain.h"
+
 
 template<typename T>
 T GetVirtualFunction(void* baseClass, const int index)
@@ -28,8 +26,9 @@ void HookLockCursor() {
     tCreateInterface ci = Interface::GetCreateInterface(GetModuleHandleA("vguimatsurface.dll"));
     surface = Interface::GetInterface<ISurface>(ci, "VGUI_Surface031");
 
-    input = **reinterpret_cast<CInput***>(PatternScanner("client.dll", "\xB9????\xF3\x0F\x11\x04\x24\xFF\x50\x10", 1).CalculateOffset(Modules::client, 0));
-
+    uintptr_t dwCinput = PatternScanner("client.dll", "\xB9????\xF3\x0F\x11\x04\x24\xFF\x50\x10", 1).CalculateOffset(Modules::client, 0);
+    std::cout << Modules::client + dwCinput << std::endl;
+    input = **reinterpret_cast<CInput***>(Modules::client + dwCinput);
     void** vTable = *reinterpret_cast<void***>(surface);
     oLockCursor = reinterpret_cast<tLockCursor>(DetourFunction(reinterpret_cast<PBYTE>(vTable[67]), reinterpret_cast<PBYTE>(hLockCursor)));
 }
