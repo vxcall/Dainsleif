9b27f87f4f3d8b2b3f045d3163da0149c8e7d92c
s3pt3mb3r <s3pt3mb3r@protonmail.com>
2020-10-26 16:54:12 +0800

    fixed a undefined symbol error
    fixed the error but there still are many problems.

--- Dainsleif/Hook/Interface.cpp
+++ Dainsleif/Hook/Interface.cpp
@@ -4,10 +4,3 @@
 tCreateInterface Interface::GetCreateInterface(const HMODULE mod) {
     return reinterpret_cast<tCreateInterface>(GetProcAddress(mod, "CreateInterface"));
 }
-
-//GetInterface returns the result of CreateInterface.
-template<class t>
-t* Interface::GetInterface(const tCreateInterface CreateInterface, const std::string& interfaceVersion) {
-    auto result = static_cast<t*>(CreateInterface(interfaceVersion.data(), nullptr));
-    return result;
-}
